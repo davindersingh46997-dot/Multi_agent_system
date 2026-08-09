559bf29f53cdc4fb578914760c51fe649f8158da
Davinder Singh <davindersingh46997@gmail.com>
2026-08-09 14:45:37 +0530

    First commit

---
+++ Face/src/App.tsx
@@ -0,0 +1,12 @@
+import ChatArea from "./pages/chat_area";
+
+function App() {
+    return (
+        <div className="min-h-screen bg-white text-gray-800">
+            <h1 className="text-3xl text-center font-bold text-gray-800">Welcome to the Chat App</h1>
+            <ChatArea />
+        </div>
+    );
+}
+
+export default App;
